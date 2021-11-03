02b2762a0860f9c2b2ecad994686f60f55da339d
cool51 <62611026+cool51@users.noreply.github.com>
2021-11-04 00:55:06 +0545

    added routes ,controller for public, player, futsaloperator

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
@@ -7,32 +7,40 @@ const app = express();
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
 
-// routers
-const authRouter = require("./routes/auth");
-const dataRouter = require("./controllers/data");
-
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// routers
+// const authRouter = require("./routes/auth");
+// const dataRouter = require("./controllers/data");
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>FUTSAL API</h1>");
-});
+const router = express.Router();
+require("./routes/auth")(app);
+require("./routes/public.routes")(app);
+require("./routes/player.routes")(app);
+require("./routes/futsalOperator.routes")(app);
+
+app.use("/", router);
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>FUTSAL API</h1>");
+// });
 
 // routes
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/data", authenticateUser, dataRouter);
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/data", authenticateUser, dataRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-
+const MONGO_URI =
+  "mongodb+srv://sauzzon:1234567890@nodeexpresscluster.xpzo6.mongodb.net/FutsalBackend?retryWrites=true&w=majority";
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
