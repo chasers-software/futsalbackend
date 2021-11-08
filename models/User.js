fa18081f913af61d9635bfb17e2be616cbcdadcf
cool51 <62611026+cool51@users.noreply.github.com>
2021-11-08 15:18:26 +0545

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -38,7 +38,7 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     required: [true, "Please provide role"],
-    enum: ["player", "operator"],
+    enum: ["player", "operator", "admin"],
     // default: "player",
   },
 });
